85167f144f585860efad299d20772533661cbc92
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-09-05 11:40:21 -0700

    Create lemonade object using shorthand creation and shortened method syntax

--- build/index.js
+++ build/index.js
@@ -1,7 +1,23 @@
 "use strict";
 
-console.log('Hello World');
-var func = function func() {
-  return 'Hi';
+var lemonJuice = 3;
+var water = 3;
+var sugar = 1.5;
+var iceCubes = 10;
+var lemonade = {
+  lemonJuice: lemonJuice,
+  water: water,
+  sugar: sugar,
+  iceCubes: iceCubes,
+  getLemonJuice: function getLemonJuice() {
+    return this.lemonJuice;
+  },
+  calculatePrice: function calculatePrice() {
+    return this.lemonJuice * 0.3 + this.water * 0.01 + this.sugar * 0.25 + this.iceCubes * 0.05 + 0.75;
+  }
 };
-console.log(func);
+console.log(lemonade.getLemonJuice());
+console.log(lemonade.calculatePrice());
+lemonade.water = 6;
+lemonade.lemonJuice = 4;
+console.log(lemonade.calculatePrice());
--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-console.log('Hello World')
+let lemonJuice = 3
+let water = 3
+let sugar = 1.5
+let iceCubes = 10
 
-const func = () => {
-  return 'Hi'
+const lemonade =  {
+  lemonJuice,
+  water,
+  sugar,
+  iceCubes,
+  getLemonJuice() {
+    return this.lemonJuice
+  },
+  calculatePrice() {
+    return (
+      this.lemonJuice * 0.3 +
+      this.water * 0.01 + 
+      this.sugar * 0.25 + 
+      this.iceCubes * 0.05 + 
+      0.75
+    )
+  }
 }
 
-console.log(func)
+console.log(lemonade.getLemonJuice())
+
+console.log(lemonade.calculatePrice())
+
+lemonade.water = 6
+lemonade.lemonJuice = 4
+
+console.log(lemonade.calculatePrice())
