6a1cbdb7fb682e43c499cdf74a5c08032d43b7dd
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-09-06 10:25:02 -0700

    Clean up code formatting

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const vorpal = Vorpal()
 vorpal
   .command(
     'createOrder <name> <phoneNumber>', 
-    'Create an order and saves it as a JSON file')
+    'Create an order and saves it as a JSON file'
+  )
   .action(function (args, callback) {
     const order = {
       total: 0,
@@ -27,13 +28,13 @@ vorpal
       }
     }
 
-    this.prompt(
-      {
-        type: 'number',
-        name: 'numLemonades',
-        default: 1,
-        message: 'How many lemonades would you like to order?'
-      }, 
+  this.prompt(
+    {
+      type: 'number',
+      name: 'numLemonades',
+      default: 1,
+      message: 'How many lemonades would you like to order?'
+    }, 
     ({ numLemonades }) => {
       let questions = []
 
@@ -42,36 +43,38 @@ vorpal
       }
       this.prompt(questions, response => {
         for (let i = 1; i <= numLemonades; i++) {
-          order = updateOrderTotal(addLemonadeToOrder(createLemonade(response, i)))
+          order = updateOrderTotal(
+            addLemonadeToOrder(createLemonade(response, i))
+          )
         }
 
-          writeFileSync(
-            order.lemonadeStand.name + '/' + order.customer.name + '.json', 
-            order
-          )
-          callback()
-        })
-      }
-    )
-  })
+        writeFileSync(
+          order.lemonadeStand.name + '/' + order.customer.name + '.json', 
+          order
+        )
+        callback()
+      })
+    }
+  )
+})
 
-  vorpal
-    .command(
-      'getOrders <lemonadeStand>',
-      'Get all orders for the given lemonade stand'
-    )
-    .action(function ({ lemonadeStand }, callback) {
-      const orders = readAllFiles(lemonadeStand)
-      this.log(`There are ${orders.length} orders at ${lemonadeStand}`)
-      for (let order of orders) {
-        this.log(`Order 1:`)
-        this.log(`Total Price: ${order.total}`)
-        this.log(`Lemonades:}`)
-        this.log(order.lemonades)
-        this.log(`Customer:`)
-        this.log(order.customer)
-      }
-      callback()
-    })
+vorpal
+  .command(
+    'getOrders <lemonadeStand>',
+    'Get all orders for the given lemonade stand'
+  )
+  .action(function ({ lemonadeStand }, callback) {
+    const orders = readAllFiles(lemonadeStand)
+    this.log(`There are ${orders.length} orders at ${lemonadeStand}`)
+    for (let order of orders) {
+      this.log(`Order 1:`)
+      this.log(`Total Price: ${order.total}`)
+      this.log(`Lemonades:}`)
+      this.log(order.lemonades)
+      this.log(`Customer:`)
+      this.log(order.customer)
+    }
+    callback()
+  })
 
 vorpal.delimiter('lemonade-stand$').show()
